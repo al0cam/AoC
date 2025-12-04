01d7933bdbb85cbc43fabedfbe54179fc9becea6
Al0cam <benjaminsikac@gmail.com>
2025-12-04 16:37:49 +0100

    4.1

---
+++ 4/src/main/kotlin/Main.kt
@@ -0,0 +1,81 @@
+package org.example
+
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import kotlin.io.path.readText
+
+fun readFile(fileName: String): ArrayList<String> {
+    val strings: ArrayList<String> = ArrayList<String>()
+    val path: Path = FileSystems.getDefault().getPath(fileName);
+    val text = path.readText()
+
+    text.split("\n").forEach { line -> strings.add(".${line.trim()}.") }
+    return strings;
+}
+
+fun getRemovableRolls(matrixRows: List<String>): Int{
+    val rollSign = '@'
+    var movableRolls = 0
+
+    // for element
+    // check adjacent units
+    // row above
+    // current row
+    // row below
+    // edges can cause issues as they don't have values
+
+    // ___
+    // .@@
+    // @@.
+    // ....@@@@.
+    val rowStart = 1 // first row is dots
+    val rowEnd = matrixRows.size-2 // last row is dots
+
+    for (row in rowStart..rowEnd) {
+        val colStart = 1 // first is dots
+        val colEnd = matrixRows[row].length-2 // last col is dots
+
+        for (col in colStart..colEnd) {
+            if (matrixRows[row][col].toChar() != rollSign)
+                continue
+            println("row: $row | col: $col")
+            var sequence = ""
+            // from current el get n-1 row, n row and n+1 row
+            println("     Sequence before: $sequence")
+            sequence += matrixRows[row-1].substring(col-1, col+2) // second index is exclusive < instead of <= ????
+            println("     Sequence n-1: $sequence")
+            sequence += matrixRows[row].substring(col-1, col+2)
+            println("     Sequence n: $sequence")
+            sequence += matrixRows[row+1].substring(col-1, col+2)
+            println("     Sequence n+1: $sequence")
+
+
+            if (sequence.count { it == rollSign } <=4) {
+                movableRolls+=1
+                println("Moving rolls increased: $movableRolls")
+            }
+        }
+
+        println()
+    }
+
+
+    return movableRolls
+}
+
+
+fun main() {
+
+    val matrixRows = readFile("./src/main/resources/file.txt")
+    val firstAndLastRow = ".".repeat(matrixRows[0].length)
+    matrixRows.addFirst(firstAndLastRow)
+    matrixRows.addLast(firstAndLastRow)
+
+    matrixRows.forEach { println("$it") }
+
+    println()
+    println("Movable rolls: ${getRemovableRolls(matrixRows)}")
+}
+
+
+// 1523
